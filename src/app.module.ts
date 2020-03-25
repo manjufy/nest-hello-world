ebca9b36d0f02428edfe00ea9c510f9bb03785ac
Manjunath Reddy <manju16832003@gmail.com>
2020-03-25 13:54:46 +0800

    Add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [ItemsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
