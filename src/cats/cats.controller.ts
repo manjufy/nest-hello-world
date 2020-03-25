b833740326a8bfc720a35846d006b979b252d100
Manjunath Reddy <manju16832003@gmail.com>
2020-03-25 11:14:02 +0800

    Add DTO and controllers

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Post, Header } from '@nestjs/common';
+import { Controller, Get, Post, Header, Body, Query } from '@nestjs/common';
+import { CreateCatDto, ListAllEntities } from './dto';
 
 @Controller('cats')
 export class CatsController {
     @Post()
     @Header('Cache-Control', 'none')
-    create(): [] {
+    async create(@Body() createCatDto: CreateCatDto) {
         return [];
     }
 
     @Get()
-    getAll(): [] {
-        return []
+    findAll(@Query() query: ListAllEntities) {
+        return `This action returns all cats (limit: ${query.limit})`;
     }
 }
---
+++ src/cats/dto.ts
@@ -0,0 +1,9 @@
+export class CreateCatDto {
+    name: string;
+    age: number;
+    breed: string;
+}
+
+export class ListAllEntities {
+    limit: number;
+}
